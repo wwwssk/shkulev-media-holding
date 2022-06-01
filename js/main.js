175f7aa7667598a2bbb1ce870decea3c6c025fa2
KostaSenatov <kostyasenatov@gmail.com>
2022-06-01 23:10:54 +0700

    .

--- js/main.js
+++ js/main.js
@@ -1,55 +1,9 @@
 /* ---------news-------------------news--------------------------news------------------ */
 
-/* 							const newsFilterBtns = $('.news__btns-filter');
-							const newswiperItems = $('.news__item');
-							const newsFilterBtn = $('.news__btn-filter');
-							function newsFilter() {
-								newsFilterBtns.on("click", event => {
-									const targetId = event.target.getAttribute('data-id')
-									const target = $(event.target)
-									
-									if (target.hasClass('news__btn-filter')) {
-										newsFilterBtn.each(function () {
-											$(this).removeClass('active');
-										})
-										target.addClass('active')
-									}
 
-									switch (targetId) {
-										case 'all':
-											getnewsItems('news__item')
-											break;
-										case 'news-new':
-											getnewsItems(targetId)
-											break;
-										case 'news-meeting':
-											getnewsItems(targetId)
-											break;
-									}
-									
-								});
-							}
-							newsFilter()
-							function getnewsItems(className) {
-								newswiperItems.each(function (index, item) {
-									if ($(this).hasClass(className)) {
-										var that = $(this);
-										setTimeout(function () {
-											that.show(400);
-										}, 0);
 
-									} else {
-										var that = $(this);
-									
-										setTimeout(function () {
-											that.hide(400);
-										}, 0);
-									}
-								});
-							} */
-
-							const windowInnerWidth = window.innerWidth;
-							if (windowInnerWidth < 769) {
+							const windowInnerWidthNews = window.innerWidth;
+							if (windowInnerWidthNews < 769) {
 								var news_initial_items = 3;
 								var news_next_items = 2;
 							} else {

805955f55efeb6ad6e1e099ca4063dde438efeb6
KostaSenatov <kostyasenatov@gmail.com>
2022-06-21 21:54:42 +0700

    .

--- js/main-script.js
+++ js/main-script.js
@@ -1,44 +1,4 @@
-/* ---------------------появление элементов при прокрутке--------------------------- */
-							function onEntry(entry) {
-								entry.forEach(change => {
-									if (change.isIntersecting) {
-										change.target.classList.add('active');
-									}
-								});
-							}
-							let options = {
-								threshold: [0.5] };
-							let observer = new IntersectionObserver(onEntry, options);
-							let elements = document.querySelectorAll('.show');
-							for (let elm of elements) {
-								observer.observe(elm);
-							}
 
-							$('.menu-header__item-projects').hover(
-								function () {
-									$('.menu-page-wrapper, .menu-header__item-projects').addClass('active');
-								},
-								function () {
-									$('.menu-page-wrapper, .menu-header__item-projects').removeClass('active');
-								}
-							);
-
-
-							$(".icon-menu-header").click(function () {
-								$('.icon-menu-header, .header, .menu-page-mobile').toggleClass("active");
-							});
-
-
-							$(document).ready(function() {
-								$('.menu-page-mobile__title-projects-main').click(function(event) {
-									$(this).toggleClass('active').next().slideToggle(600);
-									$('.menu-page-mobile__item-projects-main').toggleClass('active');
-								});
-								$('.header-mobile-title-about__spoiler').click(function(event) {
-									$(this).toggleClass('active').next().slideToggle(600);
-									$('.header-mobile-item-about__spoiler').toggleClass('active');
-								});
-							}); 
 
 /* sgp--------------------------sgp------------------------------sgp */
 							$('.item-sgp__btn-plus').click(function () {
@@ -89,59 +49,10 @@
 								$(this).toggleClass("active");
 							});
 
-var lastScroll = 0;
-jQuery(document).ready(function($) {
-	$(".page, .menu-page-mobile").addClass("indent-top");
-	$(".menu-header__item, .header__logo-click").addClass("indent-smaller");
-
-	$(window).scroll(function(){
-		var scroll = $(window).scrollTop();
-		if (scroll > lastScroll) {
-			$(".menu-header__item, .header__logo-click").removeClass("indent-smaller");
-		} else {
-			$(".menu-header__item, .header__logo-click").addClass("indent-smaller");
-		}
-		lastScroll = scroll;
-	});
-});
 
 
 
-var block_show = false;
-function scrollTracking(){
-	if (block_show) {
-		return false;
-	}
-	var wt = $(window).scrollTop();
-	var wh = $(window).height();
-	var et = $('.digits-block__row').offset().top;
-	var eh = $('.digits-block__row').outerHeight();
-	var dh = $(document).height();   
-	if (wt + wh >= et || wh + wt == dh || eh + et < wh){
-		block_show = true;
-		
-		// Код анимации
-		$(document).ready(function () {
-			$('.count').each(function () {
-				$(this).prop('Counter', 0).animate({
-					Counter: $(this).text()
-				}, {
-					duration: 1000,
-					easing: 'swing',
-					step: function (now) {
-						$(this).text(Math.ceil(now));
-					}
-				});
-			});
-		});
-	}
-}
-$(window).scroll(function(){
-	scrollTracking();
-});
-	
-$(document).ready(function(){ 
-	scrollTracking();
-});
+
+
 
 
